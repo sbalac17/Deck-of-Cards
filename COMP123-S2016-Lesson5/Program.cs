7feadf2332da648ab19e9ed1009706c528999915
Shivya B <sbalac17@my.centennialcollege.ca>
2016-06-10 09:36:23 -0400

    Lab 2

---
+++ COMP123-S2016-Lesson5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson5
+{
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {sting[]} args
+         * @returns {void}
+         */
+        public static void Main(string[] args)
+        {
+            Deck deck = new Deck(); // create a deck of cards
+            deck.Display(); // display the initial state of the deck
+
+            deck.Shuffle(); // shuffle the deck
+            deck.Display(); // display the deck after shuffling
+
+            Card cardDealt = deck.Deal();
+            Console.WriteLine("Card Delt: {0} of {1}",cardDealt.Face, cardDealt.Suit);
+            Console.WriteLine();
+            deck.Display();
+
+        } // end Main
+
+        
+
+    } // end Program
+
+} // end namespace
